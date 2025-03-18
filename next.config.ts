41e801a3f011729ba15206ab3841cafa8e0efd42
Yaseen Ahmad <yaseenahmad02.777@gmail.com>
2025-03-19 00:23:51 +0530

    change text-color

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    ignoreDuringBuilds: true, // ✅ Disable ESLint during production build
+    ignoreDuringBuilds: true, // 
   },
 };
 
